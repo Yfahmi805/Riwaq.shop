8f408864c10f3b3810fa18fdb0b67d0966efb9cc
yahya <yfahmi805@gmail.com>
2025-05-13 14:53:45 +0100

    change the categories images src

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,41 +1,47 @@
+import tshirtImage from '../assets/images/tshirt.jpeg';
+import pantsImage from '../assets/images/pants.jpeg';
+import hoodiesImage from '../assets/images/riwaqimg/img6.jpg';
+import jacketsImage from '../assets/images/jackets.jpeg';
+import dressesImage from '../assets/images/Dresses.jpeg';
+import accessoriesImage from '../assets/images/accessoirs.jpeg';
 
 const categories = [
     {
         id: 1,
         name: 'T-Shirts',
         description: '',
-        imageUrl:'src/assets/images/tshirt.jpeg'
-        
+        imageUrl: tshirtImage
     },
     {
         id: 2,
         name: 'Pants & Jeans',
         description: '',
-        imageUrl: 'src/assets/images/pants.jpeg'
+        imageUrl: pantsImage
     },
     {
         id: 3,
         name: 'Hoodies',
         description: '',
-        imageUrl: 'src/assets/images/riwaqimg/img6.jpg'
+        imageUrl: hoodiesImage
     },
     {
         id: 4,
         name: 'Jackets & Coats',
         description: '',
-        imageUrl: 'src/assets/images/jackets.jpeg'
+        imageUrl: jacketsImage
     },
     {
         id: 5,
         name: 'Dresses & Skirts',
         description: '',
-        imageUrl: 'src/assets/images/Dresses.jpeg'
+        imageUrl: dressesImage
     },
     {
         id: 6,
         name: 'Accessories',
         description: '',
-        imageUrl:'src/assets/images/accessoirs.jpeg'
+        imageUrl: accessoriesImage
     },
 ];
+
 export default categories;
